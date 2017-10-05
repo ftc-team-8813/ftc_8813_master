2c2b78cb37749412a5900a6a04022f05003ca08e
Joseph Murphy <anonymous64356@gmail.com>
2017-10-04 19:34:47 -0500

    Start on arm movement. Rotation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/tasks/TaskSomethingIDontKnow.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/tasks/TaskSomethingIDontKnow.java
@@ -3,7 +3,6 @@ package org.firstinspires.ftc.teamcode.autonomous.tasks;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-//Me gusta bailar y cantar.
 /**
  * Created by Joseph Murphy on 9/20/2017.
  */
@@ -14,14 +13,13 @@ public class TaskSomethingIDontKnow extends OpMode {
     @Override
     public void init() {
         motor = hardwareMap.dcMotor.get("motor");
-
     }
 
     @Override
     public void loop() {
         double right_y = -gamepad1.right_stick_y;
         motor.setPower(right_y);
-        if(gamepad1.right_stick_button | gamepad1.left_stick_button){
+        if(gamepad1.right_bumper | gamepad1.left_bumper){
             right_y = -.5 * gamepad1.right_stick_y;
             motor.setPower(right_y);
         }
