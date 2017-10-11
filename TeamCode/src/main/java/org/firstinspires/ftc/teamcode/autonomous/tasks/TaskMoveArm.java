d9ec46e413c8d5bdbae64f2f7f93eae07241bfb6
Joseph Murphy <anonymous64356@gmail.com>
2017-10-11 16:14:31 -0500

    Merge branch 'upstream' of https://github.com/anonymous64356/ftc_8813_master
    # Conflicts:
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/tasks/TaskSomethingIDontKnow.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/tasks/TaskMoveArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/tasks/TaskMoveArm.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.autonomous.tasks;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 /**
  * Created by Joseph Murphy on 10/4/2017.
@@ -9,9 +10,14 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 public class TaskMoveArm implements Task{
     DcMotor base;
+    Servo lowerArm;
+    Servo upperArm;
     double x;
     double y;
     double z;
+    double[] x_array = new double[2];
+    double[] y_array = new double[2];
+    double segLength = 12.0;
     public TaskMoveArm(DcMotor base, double x, double y, double z) {this.base = base; this.x = x; this.y = y; this.z = z;}
     @Override
     public void runTask() throws InterruptedException {
@@ -20,29 +26,29 @@ public class TaskMoveArm implements Task{
             Finds the cotangent of the x and y points
             and rotates the motor towards the point.
         */
-        Double tangent = Math.toDegrees(Math.atan(y/x));
+        Double tangent = Math.toDegrees(Math.atan(y / x));
         int rotation = tangent.intValue();
-        if(x >= 0 && y >= 0){
+        if (x >= 0 && y >= 0) {
             rotation = 90 - rotation;
-        }else if(x <= 0 && y >= 0){
+        } else if (x <= 0 && y >= 0) {
             rotation = 360 + rotation;
-        }else if(x >= 0 && y <= 0){
+        } else if (x >= 0 && y <= 0) {
             rotation = 180 - rotation;
-        }else if(x <= 0 && y <= 0){
+        } else if (x <= 0 && y <= 0) {
             rotation = 180 + rotation;
         }
-        if(rotation <= 180){
+        if (rotation <= 180) {
             base.setDirection(DcMotorSimple.Direction.FORWARD);
         }
-        if(rotation >= 180){
+        if (rotation >= 180) {
             base.setDirection(DcMotorSimple.Direction.REVERSE);
         }
         base.setTargetPosition(rotation);
-
-        /*
-            Extension of Arm:
-            Work in Progress
-        */
-        double XYLength = Math.sqrt((x*x) + (y*y));
+        for(int i=1; i<=2; i++){
+            double dx = x - x_array[i];
+            double dy = y - y_array[i];
+            double angle = Math.atan2(dy, dx);
+            lowerArm.setPosition(angle);
+        }
     }
 }
