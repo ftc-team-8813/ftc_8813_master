e588a61938df1a3e82bfb032c7380cf90d24cd7e
cdbbnny <aidan.yaklin@gmail.com>
2017-12-06 17:57:50 -0600

    Finish ServoPositioner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ServoPositioner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ServoPositioner.java
@@ -3,11 +3,15 @@ package org.firstinspires.ftc.teamcode.teleop;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.autonomous.util.telemetry.TelemetryWrapper;
+import org.firstinspires.ftc.teamcode.teleop.util.ButtonHelper;
+import org.firstinspires.ftc.teamcode.util.Utils;
 
 import java.lang.reflect.Array;
 import java.util.Map;
 import java.util.Set;
+import static org.firstinspires.ftc.teamcode.teleop.util.ButtonHelper.*;
 
 /**
  * ServoPositioner - Tool to move specific servos individually to collect positions
@@ -20,13 +24,18 @@ public class ServoPositioner extends OpMode {
     private int state;
     private String servo;
     private String[] servos;
+    private Servo s;
     private int chosen;
     private int scroll;
+    private double value;
+    private static final int VISIBLE_LINES = 3;
+    private ButtonHelper helper;
 
 
     @Override
     public void init() {
         state = STATE_CHOOSING;
+        helper = new ButtonHelper(gamepad1);
     }
 
     @Override
@@ -35,15 +44,47 @@ public class ServoPositioner extends OpMode {
             case STATE_CHOOSING:
                 if (servos == null) {
                     servos = keys(hardwareMap.servo.entrySet());
-                    TelemetryWrapper.setLines(4);
-                    TelemetryWrapper.setLine(0, "Choose a servo to move");
+                    TelemetryWrapper.init(telemetry, VISIBLE_LINES+1);
+                    TelemetryWrapper.setLine(0, "Choose a servo to move; press START to select");
+                } else {
+                    if (helper.pressing(dpad_up) && chosen > 0) {
+                        chosen--;
+                        if (chosen < scroll) scroll = chosen;
+                        draw();
+                    }
+                    if (helper.pressing(dpad_down) && chosen < servos.length-1) {
+                        chosen++;
+                        if (chosen > scroll + VISIBLE_LINES) scroll = chosen - VISIBLE_LINES;
+                        draw();
+                    }
+                    if (helper.pressing(start)) {
+                        servo = servos[chosen];
+                        state = STATE_RUNNING;
+                    }
                 }
-
                 break;
             case STATE_RUNNING:
-
+                if (s == null) {
+                    TelemetryWrapper.setLines(2);
+                    TelemetryWrapper.setLine(0, "Use the left joystick up/down to control the servo");
+                    s = hardwareMap.servo.get(servo);
+                }
+                value += -0.02 * gamepad1.left_stick_y;
+                value = Utils.constrain(value, 0, 1);
+                s.setPosition(value);
+                TelemetryWrapper.setLine(1, "Value: " + value);
                 break;
             default: break;
+        }
+    }
+
+    private void draw() {
+        for (int i = 0; i < VISIBLE_LINES; i++) {
+            String s = servos[scroll+i];
+            if (scroll+i == chosen) {
+                s = "> " + s;
+            }
+            TelemetryWrapper.setLine(i+1, s);
         }
     }
 
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/util/ButtonHelper.java
@@ -0,0 +1,77 @@
+package org.firstinspires.ftc.teamcode.teleop.util;
+
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+import java.util.Arrays;
+
+/**
+ * Created by aidan on 12/6/17.
+ */
+
+public class ButtonHelper {
+    private Gamepad gamepad;
+    /*
+    Buttons:
+    dpad_up dpad_down dpad_left dpad_right a b x y guide start back left_bumper right_bumper
+    left_stick_button right_stick_button (15)
+     */
+    private boolean[] buttons = new boolean[15];
+    private boolean[] held = new boolean[15];
+    /**
+     * A button for use in the pressed(), released(), and pressing() methods
+     */
+    public static final int dpad_up            =  0,
+                            dpad_down          =  1,
+                            dpad_left          =  2,
+                            dpad_right         =  3,
+                            a                  =  4,
+                            b                  =  5,
+                            x                  =  6,
+                            y                  =  7,
+                            guide              =  8,
+                            start              =  9,
+                            back               = 10,
+                            left_bumper        = 11,
+                            right_bumper       = 12,
+                            left_stick_button  = 13,
+                            right_stick_button = 14;
+
+    public ButtonHelper(final Gamepad gamepad) {
+        this.gamepad = gamepad;
+    }
+
+    private void update() {
+        boolean[] buttons2 = Arrays.copyOf(buttons, 15);
+        buttons[dpad_up] = gamepad.dpad_up;
+        buttons[dpad_down] = gamepad.dpad_down;
+        buttons[dpad_left] = gamepad.dpad_left;
+        buttons[dpad_right] = gamepad.dpad_right;
+        buttons[a] = gamepad.a;
+        buttons[b] = gamepad.b;
+        buttons[x] = gamepad.x;
+        buttons[y] = gamepad.y;
+        buttons[guide] = gamepad.guide;
+        buttons[start] = gamepad.start;
+        buttons[back] = gamepad.back;
+        buttons[left_bumper] = gamepad.left_bumper;
+        buttons[right_bumper] = gamepad.right_bumper;
+        buttons[left_stick_button] = gamepad.left_stick_button;
+        buttons[right_stick_button] = gamepad.right_stick_button;
+        for (int i = 0; i < 15; i++) {
+            held[i] = buttons[i] && buttons2[i];
+        }
+    }
+
+    public boolean pressed(int idx) {
+        update();
+        return buttons[idx];
+    }
+
+    public boolean released(int idx) {
+        return !pressed(idx);
+    }
+
+    public boolean pressing(int idx) {
+        return pressed(idx) && !held[idx];
+    }
+}
