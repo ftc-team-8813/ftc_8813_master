d8750e966af4ba7b19c5f0368e64aa5587af7746
cdbbnny <aidan.yaklin@gmail.com>
2017-12-16 06:26:19 -0600

    Update EncoderTest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/test/EncoderTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/test/EncoderTest.java
@@ -3,16 +3,17 @@ package org.firstinspires.ftc.teamcode.autonomous.test;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.teamcode.autonomous.BaseAutonomous;
 import org.firstinspires.ftc.teamcode.autonomous.util.DcMotorUtil;
 
 /**
  * Created by aidan on 12/14/17.
  */
 @Autonomous(name="Encoder Test")
-public class EncoderTest extends LinearOpMode {
+public class EncoderTest extends BaseAutonomous {
 
     @Override
-    public void runOpMode() throws InterruptedException {
+    public void run() throws InterruptedException {
         DcMotorUtil.moveToPosition(
                 hardwareMap.dcMotor.get("base"),
                 DcMotorUtil.degreesToEncoders(180, 60),
