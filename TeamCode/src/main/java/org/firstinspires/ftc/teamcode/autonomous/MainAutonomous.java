2b0efb4535c572a2238207ece7dcbdb6aa52d23f
Joseph Murphy <anonymous64356@gmail.com>
2018-01-25 19:51:56 -0600

    Merge branch 'upstream' of https://github.com/anonymous64356/ftc_8813_master
    # Conflicts:
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MainTeleOp.java
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/util/ArmDriver.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/MainAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/MainAutonomous.java
@@ -66,7 +66,7 @@ public abstract class MainAutonomous extends BaseAutonomous {
         //Set result to NONE if result is null
         if (result == null) result = TaskClassifyPictograph.Result.NONE;
         //Place glyph
-        tasks.add(new TaskPlaceGlyphAutonomous(quadrant(), result, base, arm));
+        tasks.add(new TaskPlaceGlyphAutonomousSimple(quadrant(), result, base, arm));
         //Knock jewel
         if (COLOR_SENSOR) tasks.add(new TaskScoreJewel(quadrant()));
     }
