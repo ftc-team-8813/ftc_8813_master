c6c35efe7f5658731f45b3b359f4a2b3ee0e997d
cdbbnny <aidan.yaklin@gmail.com>
2018-10-09 17:05:39 -0500

    Add color detection and recognition

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/ColorCounter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/ColorCounter.java
@@ -1,36 +1,88 @@
 package org.firstinspires.ftc.teamcode.autonomous;
 
+import android.app.Activity;
 import android.graphics.Color;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
+import com.qualcomm.hardware.ams.AMSColorSensor;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 
+import org.firstinspires.ftc.robotcontroller.internal.FtcRobotControllerActivity;
+import org.firstinspires.ftc.robotcore.internal.system.AppUtil;
 import org.firstinspires.ftc.teamcode.autonomous.tasks.Task;
+import org.firstinspires.ftc.teamcode.util.ColorRange;
+import org.firstinspires.ftc.teamcode.util.Logger;
+import org.opencv.android.JavaCameraView;
 
 @Autonomous(name="Color Test")
 public class ColorCounter extends BaseAutonomous
 {
     private ColorSensor sensor;
+    private volatile boolean uiRunning;
+    private Logger log = new Logger("Color Sense");
+    private View colorView;
+    private Activity a;
     
     @Override
     public void run() throws InterruptedException
     {
-        tasks.add(new Task()
+        a = AppUtil.getInstance().getActivity();
+        FtcRobotControllerActivity rc = (FtcRobotControllerActivity) a;
+        final LinearLayout cameraLayout = rc.cameraMonitorLayout;
+    
+        while (uiRunning)
+        {
+            try
+            {
+                Thread.sleep(0, 100);
+            } catch (InterruptedException e) {
+                log.w("Ignoring interrupt");
+            }
+        }
+    
+        sensor = hardwareMap.colorSensor.get("color");
+        telemetry.addData("Color sensor address", sensor.getI2cAddress());
+        while (opModeIsActive())
+        {
+            float[] hsv = new float[3];
+            NormalizedRGBA rgba = ((AMSColorSensor)sensor).getNormalizedColors();
+            final int r = (int)(Math.min(255, Math.log(rgba.red+1)*5000));
+            final int g = (int)(Math.min(255, Math.log(rgba.green+1)*6000));
+            final int b = (int)(Math.min(255, Math.log(rgba.blue+1)*6000));
+            Color.colorToHSV(Color.rgb(r, g, b), hsv);
+            telemetry.addData("Hue", hsv[0]);
+            telemetry.addData("Saturation", hsv[1]);
+            telemetry.addData("Value", hsv[2]);
+            telemetry.addData("Red", r);
+            telemetry.addData("Green", g);
+            telemetry.addData("Blue", b);
+            telemetry.addData("Color", ColorRange.toString(ColorRange.getColor(r, g, b)));
+            telemetry.update();
+            a.runOnUiThread(new Runnable()
+            {
+                @Override
+                public void run()
+                {
+                    cameraLayout.setBackgroundColor(Color.rgb(r, g, b));
+                }
+            });
+            Thread.sleep(10);
+        }
+        
+        uiRunning = true;
+        a.runOnUiThread(new Runnable()
         {
             @Override
-            public void runTask() throws InterruptedException
+            public void run()
             {
-                sensor = hardwareMap.colorSensor.get("color");
-                telemetry.addData("Color sensor address", sensor.getI2cAddress());
-                while (opModeIsActive())
-                {
-                    float[] hsv = new float[3];
-                    Color.colorToHSV(Color.rgb(sensor.red(), sensor.green(), sensor.blue()), hsv);
-                    telemetry.addData("Hue", hsv[0]);
-                    telemetry.addData("Saturation", hsv[1]);
-                    telemetry.addData("Value", hsv[2]);
-                }
+                cameraLayout.setBackgroundColor(0);
+                uiRunning = false;
             }
         });
+        while (uiRunning) ;
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/ColorRange.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/ColorRange.java
@@ -5,19 +5,46 @@ import android.graphics.Color;
 public class ColorRange
 {
     public static final int BLACK  = 0;
-    public static final int WHITE  = 1;
-    public static final int RED    = 2;
-    public static final int ORANGE = 3;
-    public static final int YELLOW = 4;
-    public static final int GREEN  = 5;
-    public static final int BLUE   = 6;
-    public static final int PURPLE = 7;
+    public static final int GRAY   = 1;
+    public static final int WHITE  = 2;
+    public static final int RED    = 3;
+    public static final int ORANGE = 4;
+    public static final int YELLOW = 5;
+    public static final int GREEN  = 6;
+    public static final int BLUE   = 7;
+    public static final int PURPLE = 8;
+    private static final String[] colors = {
+            "Black", "Gray", "White", "Red", "Orange", "Yellow", "Green", "Blue", "Purple"
+    };
     
     public static int getColor(int red, int green, int blue)
     {
         float[] hsv = new float[3];
         Color.colorToHSV(Color.rgb(red, green, blue), hsv);
-        
-        return 0;
+        float h = hsv[0];
+        float s = hsv[1];
+        float v = hsv[2];
+        if (s < .4 || v < .3)
+        {
+            if      (v > .8) return WHITE;
+            else if (v > .3) return GRAY;
+            else             return BLACK;
+        }
+        else
+        {
+            if      (h < 20)  return RED;
+            else if (h < 25)  return ORANGE;
+            else if (h < 75)  return YELLOW;
+            else if (h < 165) return GREEN;
+            else if (h < 270) return BLUE;
+            else if (h < 315) return PURPLE;
+            else              return RED;
+        }
+    }
+    
+    public static String toString(int col)
+    {
+        if (col < 0 || col > colors.length) return "Unknown";
+        return colors[col];
     }
 }
