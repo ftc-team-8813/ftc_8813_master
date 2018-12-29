562591b82cca9cb8ff9e28d212bbb30766436d67
cdbbnnyCode <aidan.yaklin@gmail.com>
2018-12-29 10:09:46 -0600

    Move StupidAutonomous out of autonomous.util

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/StupidAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/StupidAutonomous.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.autonomous.util;
+package org.firstinspires.ftc.teamcode.autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
