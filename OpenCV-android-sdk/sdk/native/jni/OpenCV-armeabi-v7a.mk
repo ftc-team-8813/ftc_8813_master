ddf2634e39d8796d3b47db3ab6c2b8895c60297d
cdbbnnyCode <aidan.yaklin@gmail.com>
2019-07-01 16:22:56 -0500

    Move OpenCV SDK to a submodule

--- OpenCV-android-sdk/sdk/native/jni/OpenCV-armeabi-v7a.mk
+++
@@ -1,2 +0,0 @@
-OPENCV_3RDPARTY_COMPONENTS:=tbb cpufeatures libprotobuf libjpeg-turbo libwebp libpng libtiff libjasper IlmImf quirc tegra_hal
-OPENCV_EXTRA_COMPONENTS:=/home/aidan/Android/Sdk/ndk-bundle/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/arm-linux-androideabi/16/libz.so dl m log
