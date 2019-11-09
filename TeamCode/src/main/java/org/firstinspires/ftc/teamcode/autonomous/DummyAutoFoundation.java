b2bc6930278c1a6ebeaf42ff941001203f692b91
Siddapa <vishnuschalla@gmail.com>
2019-11-08 21:30:42 -0600

    Pre-League Autonomous for Both Sides of Blue

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/DummyAutoFoundation.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.common.Robot;
+import org.firstinspires.ftc.teamcode.common.actuators.Drivetrain;
+
+
+@Autonomous(name="DummyAutoFoundation")
+public class DummyAutoFoundation extends BaseAutonomous
+{
+    @Override
+    public void run() throws InterruptedException
+    {
+        Robot robot = Robot.instance();
+        robot.drivetrain.drive(-0.2, 0, 0);
+        Thread.sleep(3000);
+        robot.drivetrain.drive(0, 0, 0);
+    }
+}
